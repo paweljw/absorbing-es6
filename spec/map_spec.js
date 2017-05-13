fdd343f030d6708440cea37fa4a0518c3fd3e330
Paweł J. Wal <pawel.wal@codesthq.com>
2017-05-13 10:59:50 +0200

    Plucking with map

--- spec/map_spec.js
+++ spec/map_spec.js
@@ -1,10 +1,18 @@
 /* global describe, it, expect */
 
-let numbers = [ 1, 2, 3 ]
-
 describe('map', () => {
   it('doubles the numbers', () => {
+    let numbers = [ 1, 2, 3 ]
     let doubledNumbers = numbers.map(number => { return number * 2 })
     expect(doubledNumbers).toEqual([2, 4, 6])
+  })
+
+  it('plucks properties', () => {
+    let cars = [
+      {name: 'Buick', price: 'cheap'},
+      {name: 'Camaro', price: 'expensive'}
+    ]
+    let prices = cars.map(car => { return car.price })
+    expect(prices).toEqual(['cheap', 'expensive'])
   })
 })
