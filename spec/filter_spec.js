ac86210bd18ccc7744ffbe37b9efb865488219a0
Paweł J. Wal <pawel.wal@codesthq.com>
2017-05-13 11:15:07 +0200

    Slightly more complicated filter

--- spec/filter_spec.js
+++ spec/filter_spec.js
@@ -7,6 +7,13 @@ let produce = [
   {name: 'orange', type: 'fruit'}
 ]
 
+let complicatedProduce = [
+  {name: 'cucumber', type: 'vegetable', quantity: 0, price: 1},
+  {name: 'banana', type: 'fruit', quantity: 10, price: 15},
+  {name: 'celery', type: 'vegetable', quantity: 30, price: 13},
+  {name: 'orange', type: 'fruit', quantity: 3, price: 5}
+]
+
 describe('filter', () => {
   it('filters the vegetables', () => {
     let vegetables = produce.filter(product => { return product.type === 'vegetable' })
@@ -21,5 +28,15 @@ describe('filter', () => {
   it('drops the celery', () => {
     let fruit = produce.filter(product => { return product.name !== 'celery' })
     expect(fruit).toEqual([{name: 'cucumber', type: 'vegetable'}, {name: 'banana', type: 'fruit'}, {name: 'orange', type: 'fruit'}])
+  })
+
+  it('selects vegetables which are in stock with a price less than 10', () => {
+    let cart = complicatedProduce.filter(product => { return product.type === 'vegetable' && product.quantity > 0 && product.price < 10 })
+    expect(cart).toEqual([])
+  })
+
+  it('selects vegetables which are in stock with a price more than 10', () => {
+    let cart = complicatedProduce.filter(product => { return product.type === 'vegetable' && product.quantity > 0 && product.price > 10 })
+    expect(cart).toEqual([{name: 'celery', type: 'vegetable', quantity: 30, price: 13}])
   })
 })
