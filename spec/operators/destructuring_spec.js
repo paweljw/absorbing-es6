bb466697741b4482ec746fc267273f090cc055ba
Paweł J. Wal <pawel.wal@codesthq.com>
2017-05-15 18:28:43 +0200

    Destructuring arrays

--- spec/operators/destructuring_spec.js
+++ spec/operators/destructuring_spec.js
@@ -6,12 +6,35 @@ function expenseSentence ({ type, amount }) {
   return `Expense of ${amount} to ${type}`
 }
 
+const color = { color: 'red' }
+
+function expenseSentenceWithColor ({ type, amount }, { color }) {
+  return `Expense of ${amount} to ${type}, color ${color}`
+}
+
+const companies = [ 'Facebook', 'Google', 'Uber' ]
+
 describe('destructuring', () => {
-  it('destructures', () => {
+  it('destructures an object', () => {
     const { type, amount } = expense
     expect(type).toBe('Debit')
     expect(amount).toBe('$45')
   })
 
   it('destructures function arguments', () => expect(expenseSentence(expense)).toBe('Expense of $45 to Debit'))
+  it('destructures function arguments fancily', () => {
+    expect(expenseSentenceWithColor(expense, color)).toBe('Expense of $45 to Debit, color red')
+  })
+  it('destructures companies off of array', () => {
+    const [ name, name2, name3 ] = companies
+    expect(name).toBe('Facebook')
+    expect(name2).toBe('Google')
+    expect(name3).toBe('Uber')
+  })
+
+  it('destructures companies with rest', () => {
+    const [ name, ...rest ] = companies
+    expect(name).toBe('Facebook')
+    expect(rest).toEqual(['Google', 'Uber'])
+  })
 })
