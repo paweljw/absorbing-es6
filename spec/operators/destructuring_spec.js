47bdcf4d9c07d46ec34479f51e84d299867e1938
Paweł J. Wal <pawel.wal@codesthq.com>
2017-05-15 18:21:30 +0200

    Destructuring function arguments

--- spec/operators/destructuring_spec.js
+++ spec/operators/destructuring_spec.js
@@ -2,10 +2,16 @@
 
 const expense = { type: 'Debit', amount: '$45' }
 
+function expenseSentence ({ type, amount }) {
+  return `Expense of ${amount} to ${type}`
+}
+
 describe('destructuring', () => {
   it('destructures', () => {
     const { type, amount } = expense
     expect(type).toBe('Debit')
     expect(amount).toBe('$45')
   })
+
+  it('destructures function arguments', () => expect(expenseSentence(expense)).toBe('Expense of $45 to Debit'))
 })
